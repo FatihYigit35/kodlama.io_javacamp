9962e3a21ef52428d143015f16254deccf04cdf5
Bukrek35TR <fatih.yigit.35@gmail.com>
2022-10-02 13:46:53 +0300

    Update Main.java

--- week_01/javaDemos/variables/Main.java
+++ week_01/javaDemos/variables/Main.java
@@ -3,7 +3,7 @@ package variables;
 public class Main {
 
 	public static void main(String[] args) {
-		int ogrenciSayisi = 12;
+		int ogrenciSayisi = 88;
 		String mesaj = "Öğrenci sayısı : ";
 		
 		System.out.println(mesaj + ogrenciSayisi);
