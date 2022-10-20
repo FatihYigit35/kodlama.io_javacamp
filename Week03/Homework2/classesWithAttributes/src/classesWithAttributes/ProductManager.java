99e0e75652447b7699ea81a3e674b9d9759987a0
Bukrek35TR <fatih.yigit.35@gmail.com>
2022-10-20 23:59:48 +0300

    Week03 - Homework2

---
+++ Week03/Homework2/classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,14 @@
+package classesWithAttributes;
+
+public class ProductManager {
+	public void add(Product product) {
+		//JDBC
+		System.out.println("Ürün eklendi. < " + product.getName());
+	
+	}
+	
+	public void add2() {
+		
+	}
+
+}
